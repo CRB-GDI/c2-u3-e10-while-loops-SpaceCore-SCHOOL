fca7f8ac81644ec40d64f79ddb3f634005887847
SpaceCore (Matthew Terpening) <145371453+SpaceCore-SCHOOL@users.noreply.github.com>
2024-03-14 12:20:58 -0400

    Completed Problem 1

--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -29,7 +29,36 @@
             {
                 Console.WriteLine("Closing game.");
             }
+            while (response < 4)
+            {
+                Console.WriteLine("**************");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("**************");
+                Console.WriteLine("Make your selection now!");
+                response = int.Parse(Console.ReadLine());
+                if (response == 1)
+                {
+                    Console.WriteLine("Starting a new game!");
+                }
+                if (response == 2)
+                {
+                    Console.WriteLine("File loading!");
+                }
+                if (response == 3)
+                {
+                    Console.WriteLine("Loading settings...");
+                }
+                if (response == 4)
+                {
+                    Console.WriteLine("Closing game.");
+                }
 
             }
         }
+
     }
+}
